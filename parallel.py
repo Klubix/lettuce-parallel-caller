524d81806436086b3fc43e0c87fc2532004871dc
Klubix <klubix36@gmail.com>
2017-10-17 17:13:52 +0200

    First base parallel feature running version.

--- parallel.py
+++ parallel.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import datetime
+from logging import Logger
 from multiprocessing import Pool, cpu_count
 from subprocess import Popen, PIPE
 from itertools import product
@@ -8,27 +9,34 @@ from itertools import product
 TAGS = {'id=test1', 'id=test2', 'id=test3', 'id=test4', 'id=test5'}
 
 
-class LoggerToFile:
+# Out of class due to Pool.map limitations on calling function inside a class
+def unpack_run_features_function(args):
+    return Feature().run_features(*args)
+
+
+class CustomLogger(Logger):
 
     def __init__(self):
-        pass
+        self._log_template = "PID: {0} - {1}"
+        self._logs_path = "log/{0}"
+        self._log_file_template = "{0}/{1}.txt"
+        super(CustomLogger, self).__init__("CustomLogger")
 
     def prepare_logs_folder(self):
         current_date_and_time = datetime.datetime.now()
-        logs_path = "log/{0}".format(current_date_and_time.strftime("%Y-%m-%dT%H_%M"))
+        logs_path = self._logs_path.format(current_date_and_time.strftime("%Y-%m-%dT%H_%M"))
         if not os.path.exists(logs_path):
             os.makedirs(logs_path)
         return logs_path
 
-    def log_output(self, stdout, log_directory, tag_arguments, pid):
-        # TODO: split logging to console and file
-        log_file = open('{0}/{1}.txt'.format(log_directory, tag_arguments), 'w')
+    def log_output_to_console_and_file(self, stdout, log_directory, tag_arguments, pid):
+        log_file = open(self._log_file_template.format(log_directory, tag_arguments), 'w')
         for line in stdout:
-            sys.stdout.write("PID: {0} - {1}".format(pid, line))
-            log_file.write(line)
+            log_file.write(self._log_template.format(pid, line))
+            sys.stdout.write(self._log_template.format(pid, line))
 
 
-class Tag:
+class Tag(object):
 
     def __init__(self):
         pass
@@ -42,19 +50,16 @@ class Tag:
         return tags_lists_dict
 
 
-class Feature:
+class Feature(object):
 
     def __init__(self):
         pass
-
-    def run_features_unpack(self, args):
-        return self.run_features(*args)
 
     def run_features(self, tags_list, log_directory):
         tag_parameters = self.prepare_tags_parameters(tags_list)
         command = 'lettuce features {0}'.format(tag_parameters)
         process = ParallelProcess().run_process(command.split())
-        LoggerToFile().log_output(process.stdout, log_directory, tag_parameters, process.pid)
+        CustomLogger().log_output_to_console_and_file(process.stdout, log_directory, tag_parameters, process.pid)
         return process.wait()
 
     def prepare_tags_parameters(self, tags_list):
@@ -66,33 +71,29 @@ class Feature:
                 tag_parameters += ' {0}'.format(tag_template.format(tags_list[i]))
         return tag_parameters
 
-# TODO: remove after tests
-def run_features_unpack_2(args):
-   return Feature().run_features(*args)
 
-class ParallelProcess:
+class ParallelProcess(object):
 
     def __init__(self):
         pass
 
     def open_parallel_processes(self, processes_count, function, iterable):
         pool = Pool(processes=processes_count)
-        # TODO: fix function call
-        # https://stackoverflow.com/questions/3288595/multiprocessing-how-to-use-pool-map-on-a-function-defined-in-a-class
         pool.map(function, iterable)
+
 
     def run_process(self, process_command):
         process = Popen(process_command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
         return process
 
 
-class Iterable:
+class Iterable(object):
 
     def __init__(self):
         pass
 
     def make_iterable_from_string(self, string):
-        return {0: string}.values()
+        return [string]
 
     def make_product_iterable(self, *args):
         return product(*args)
@@ -101,8 +102,8 @@ class Iterable:
 if __name__ == '__main__':
     processes_count = cpu_count()
     tags_lists_dict = Tag().prepare_dict_with_tags_chunks(list(TAGS), processes_count)
-    logs_path = LoggerToFile().prepare_logs_folder()
+    logs_path = CustomLogger().prepare_logs_folder()
     logs_path_iterable = Iterable().make_iterable_from_string(logs_path)
     iterable_function_arguments = Iterable().make_product_iterable(tags_lists_dict.values(), logs_path_iterable)
 
-    ParallelProcess().open_parallel_processes(processes_count, Feature().run_features_unpack.im_func, iterable_function_arguments)
+    ParallelProcess().open_parallel_processes(processes_count, unpack_run_features_function, iterable_function_arguments)
